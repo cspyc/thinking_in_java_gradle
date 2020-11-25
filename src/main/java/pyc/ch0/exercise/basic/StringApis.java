3faee5c5c2f97f35faac37ad71c61f7462fa18ca
PiYicheng <cspyc@foxmail.com>
2020-11-26 00:11:24 +0800

    clean cache and add gitIngnore

---
+++ src/main/java/pyc/ch0/exercise/basic/StringApis.java
@@ -0,0 +1,12 @@
+package pyc.ch0.exercise.basic;
+
+/**
+ * @author pi
+ * @date 2020/11/24 11:26 PM
+ **/
+public class StringApis {
+
+    public int compareTo(String a, String z) {
+        return a.compareTo(z);
+    }
+}
---
+++ src/test/java/pyc/ch0/exercise/basic/StringApisTest.java
@@ -0,0 +1,19 @@
+package pyc.ch0.exercise.basic;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author pi
+ * @date 2020/11/24 11:24 PM
+ **/
+public class StringApisTest {
+
+    @Test
+    public void should_return_true_when_a_compare_to_z() throws Exception {
+        StringApis stringApis = new StringApis();
+        assertThat(stringApis.compareTo("a","z"), greaterThan(0));
+    }
+}
