40086f7d03c8012a458b589f8f9d293245e70691
PiYicheng <cspyc@foxmail.com>
2020-12-09 22:42:10 +0800

    datastuct

---
+++ src/main/java/pyc/datastructandalgorithm/CustomStack.java
@@ -0,0 +1,10 @@
+package pyc.datastructandalgorithm;
+
+/**
+ * @author pi
+ * @date 2020/12/08 11:44 PM
+ **/
+public class CustomStack {
+    public CustomStack() {
+    }
+}
---
+++ src/test/java/pyc/datastructandalgorithm/StackTest.java
@@ -0,0 +1,14 @@
+package pyc.datastructandalgorithm;
+
+import org.junit.Test;
+
+/**
+ * @author pi
+ * @date 2020/12/08 10:42 PM
+ **/
+public class StackTest {
+    @Test
+    public void should_create_stack_object() throws Exception {
+        CustomStack customStack = new CustomStack();
+    }
+}
